700da3c2a53f04fad8a9ba92d47b058d00955b22
SLRE <isc.slre@gmail.com>
2016-11-01 12:17:52 -0600

    First Commit

---
+++ commands.js
@@ -0,0 +1,315 @@
+git clone -b <branch> <remote_repo>
+javascript:leaveHasbroPopup('/local-local/leave?new_win=true&url=address')
+
+sudo vim /etc/php.ini : /max_execution_time
+i 600 :wq
+.provision/shell/install.sh
+and add npm install -g bower
+
+and add config.ssh.forward_agent = true;
+intro the Vagrant.cofigure(“2”) do |config|
+
+
+composer.json
+and add "secure-http":false, intro config
+INSTALLATION
+
+vagrant up 
+
+vagrant ssh
+
+cd /vagrant
+
+composer install
+
+
+
+git clone git@gitlab.hosting-hasbro.com:brand-sites/hasbro-com.git plyd
+
+git clone NERF-1216-ENRIQUE  git@gitlab.hosting-hasbro.com:brand-sites/nerf.git NERF
+
+
+
+
+PLYD-809-EDWIN
+
+var jquery = document.createElement('script'); 
+jquery.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+document.head.appendChild(jquery);
+
+
+var fileref = document.createElement("script");
+fileref.setAttribute("type", "text/javascript");
+fileref.setAttribute("src", "http://code.jquery.com/jquery-latest.min.js");
+document.head.appendChild(fileref);
+
+ $("[data-machine-name]").each(function(){
+
+console.log($(this)[0].attributes[1])
+
+})
+ 
+ $("[data-machine-name]").each(function(){
+
+console.log(this)
+
+})
+
+$( ".inner-wrapper" ).addClass( "large" );
+
+$('#assetsGrid tbody tr').each(function(i,v){
+  
+  if(($(v)).find('input').is(':checked')){
+    
+  console.log(($(v).find("[aria-describedby=assetsGrid_NAME]"))[0].textContent)
+  console.log(($(v).find("[aria-describedby=assetsGrid_PRIMARYBRANDLABEL]"))[0].textContent)
+  console.log(($(v).find("[aria-describedby=assetsGrid_MEDIATYPENAME]"))[0].textContent)
+    
+  }
+  
+})
+
+
+(function(){
+  var newscript = document.createElement('script');
+     newscript.type = 'text/javascript';
+     newscript.async = true;
+     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+})();
+
+$('section.grid .content .item').each(function(i,v){
+  var str = $(this).attr('href');
+  str= str.substr(str.indexOf(":") + 1);
+ 
+  console.log($(this).find('.item_description').text() +' : '+str)            
+})
+http://helios.hasbro.com/admin/helios/index.cfm?event=product.locale.form&id= 
+
+
+for i in `vagrant global-status | grep virtualbox | awk '{ print $1 }'` ; do vagrant destroy $i ; done
+{if $smarty.server.REQUEST_URI|strstr:"/{$locale}/movies/"}
+$smarty.server.REQUEST_URI ==  "/{$locale}/rescue-bots/videos"
+if(!(string.indexOf(substring) > -1)){
+	
+	
+	
+	
+}
+
+/*-------------INDEXOF SMARTY-------------------*/
+{if $smarty.server.REQUEST_URI|strstr:"/{$locale}/movies/"}
+{getTranslation text="Beyblade Burst App"}
+{/if}
+
+labels in (Production, Production_BulkUpdate) AND status in (Open, "IN PROGRESS", Reopened, Blocked, "Information Requested", Accepted) ORDER BY due ASC, updated 
+
+
+git clone -b MLP-DEV-FIX  git@gitlab.hosting-hasbro.com:brand-sites/mylittlepony-2015.git MLP-DEV-FIX
+
+function getAttributes ( $node ) {
+  var attrs = {};
+  $.each( $node[0].attributes, function ( index, attribute ) {
+      attrs[attribute.name] = attribute.value;
+  } );
+
+  return attrs;
+}
+
+$.ajax({
+  //url: 'http://api.brightcove.com/services/library?command=find_playlist_by_id&playlist_id=' + '5095660792001' + '&playlist_fields=id%2Cname%2CvideoIds&token=' + 'giuCDwBmr7luk1iDJYb-BiNlOXPyrywE4okjjLmKsUtITwa5IqtH9Q..' + '&',
+  url: 'http://api.brightcove.com/services/library?command=find_video_by_id&video_id=5095660792001&video_fields=videoStillURL&media_delivery=default&callback=BCL.onSearchResponse&token=giuCDwBmr7luk1iDJYb-BiNlOXPyrywE4okjjLmKsUtITwa5IqtH9Q..',
+  dataType: 'jsonp',
+  success: function (jsonp) {
+    console.log(result);
+  }
+  
+  api.brightcove.com/services/library?command=find_video_by_id&video_id=5095660792001&token=giuCDwBmr7luk1iDJYb-BiNlOXPyrywE4okjjLmKsUtITwa5IqtH9Q..
+  
+  
+  
+  $.ajax({
+              url:'//api.brightcove.com/services/library?command=find_playlist_by_id&playlist_id=4924650833001&token=giuCDwBmr7luk1iDJYb-BiNlOXPyrywE4okjjLmKsUtITwa5IqtH9Q..',
+              dataType: 'jsonp',
+              success: function (result) {
+                console.log(result);
+                
+              }
+            });
+  
+  
+  
+  if(window.location.pathname.indexOf('/brands/beyblade/')!=-1){
+     /**/
+     var addTumb = function(){
+       var tWidth = $('.hero-brightcove-video').attr('width');
+       var tHeight = $('.hero-brightcove-video').attr('height');
+         $('<div id ="thumbVideoContainer" style="width:'+tWidth+'px; height:'+tHeight+'px; background-color:rgba(0, 0, 0, 0.5); position:absolute;"></div>').insertBefore( ".hero-brightcove-video" );
+         $('#thumbVideoContainer').click(function(){
+			   $(this).hide();
+			   var dataBegin = $('.hero-brightcove-video').attr('data');
+			   var dataEnd ='';
+			   dataEnd = dataBegin.replace('&autoStart=false','&autoStart=true');
+			   $('.hero-brightcove-video').attr('data',dataEnd);
+         });
+         //BrightCoveAPI.pause(player.attr("id"));
+         /*{$bc_token = $site_config['bc_read_token']}
+         var bc_token = "{$bc_token}";
+         $.ajax({
+          url:'//api.brightcove.com/services/library?command=find_video_by_id&video_id='+videoId+'&token='+bc_token,
+          dataType: 'jsonp',
+          success: function (result) {
+                      console.log(result);*/
+                    }
+          });
+
+     };
+     setTimeout(addTumb, 5000);
+
+   }
+ /*-------------BC AUTOSTART-------------------*/  
+   if(window.location.pathname.indexOf('/brands/beyblade/')!=-1){
+         videoAutoStart =false;
+            /*var dataBegin = $('.hero-brightcove-video').attr('data');
+            var dataEnd ='';
+            dataEnd = dataBegin.replace('&autoStart=true','&autoStart=false');
+            $('.hero-brightcove-video').attr('data',dataEnd);*/
+
+          }
+		  
+/*-------------GET LOCALE-------------------*/		  
+var c_url = "http://hasbro.qa4.hweb.com/en-us/brands/beyblade#";
+var regexLocale = /[\/][a-z][a-z][-][a-z][a-z]\//g;
+var match = regexLocale.exec(c_url);
+	  
+var c_url = window.location.pathname;
+  var regexLocale = /[\/][a-z][a-z][-][a-z][a-z]\//g;
+  var match = regexLocale.exec(c_url);
+  var regexLocale2 = /[a-z][a-z][-][a-z][a-z]/g;
+var match2 = regexLocale2.exec(match[0]);
+
+match2[0]
+
+
+
+
+api.brightcove.com/services/library?command=find_playlist_by_id&playlist_id=4924650833001&playlist_fields=shortDescription&video_fields=&media_delivery=default&token=giuCDwBmr7luk1iDJYb-BiNlOXPyrywE4okjjLmKsUtITwa5IqtH9Q..
+--------------
+
+ $.ajax({
+              url:'//api.brightcove.com/services/library?command=find_playlist_by_id&playlist_id='+pl_id+'&token='+bc_token+"'",
+              dataType: 'jsonp',
+              success: function (result) {
+                console.log(result);
+                
+              }
+            });
+
+			
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+/*-------------IPHONE-------------------*/
+var iOS = /iPhone/.test(navigator.userAgent) && !window.MSStream;
+if(iOS){
+	$(window).on("orientationchange",function(){
+	});
+}
+
+/*-------------SAFARI-------------------*/
+var isSafari = navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&  navigator.userAgent && !navigator.userAgent.match('CriOS');
+				   
+if(isSafari){
+	$(window).on('resize',function(){location.reload();});
+	
+}
+
+
+/*-------------SCROLL-------------------*/
+
+$('.scroll-icon-container a').click(function(e){
+		   e.preventDefault();
+		   // get id of target div (placed in href attribute of anchor element)
+		   // and pass it to the scrollToElement function
+		    // also, use 2000 as an argument for the scroll speed (2 seconds, 2000 milliseconds)
+		   scrollToElement( $(this).attr('href'), 500 );
+		});
+		var scrollToElement = function(el, ms){
+		    var speed = (ms) ? ms : 600;
+		    $('html,body').animate({
+		        scrollTop: $(el).offset().top
+		    }, speed);
+		}	
+	
+	
+}
+
+/*------MILISECONDS TO TIME----*/			
+
+
+
+
+
+
+/*----CHECK BROWSER----*/
+
+    // Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+    // Safari <= 9 "[object HTMLElementConstructor]" 
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    // Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+    // Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+    // Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+    // Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+
+https://openload.co/embed/Byv16Sd0eu4/
+
+
+
+/*------------Get url params value by name -------------*/
+
+function gup( name, url ) {
+    if (!url) url = location.href;
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var results = regex.exec( url );
+    return results == null ? null : results[1];
+}
+gup('q', 'hxxp://example.com/?q=abc')
+
+
+
+
+/**   Balanced    **/
+
+var string="((({{{[[{}]]}}})))";
+string = string.split("")
+var index = (string.length/2) -1
+for(var i=0;i<index+1;i++){
+ getLetter(i) 
+}
+function getLetter(value){
+  console.log(string[index-value])
+  console.log(string[value+index+1])
+}
